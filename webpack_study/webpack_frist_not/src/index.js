8d7494b36a3281fcf8002d77161ef499f0038fee
OldYoung <mmzcg@outlook.com>
2022-05-07 17:23:58 +0800

    webpack 使用

--- webpack_study/webpack_frist_not/src/index.js
+++ webpack_study/webpack_frist_not/src/index.js
@@ -1,8 +1,12 @@
 // 使用ES6的导入语法,导入jQuery
 import $ from 'jquery'
 
+// 导入样式
+import "./css/index.css"
+
+import "./css/index.less"
 
 $(function(){
     $("li:odd").css("background-color","red")
-    $("li:even").css("background-color","pink")
+    $("li:even").css("background-color","gray")
 })
--- webpack_study/webpack_frist_not/webpack.config.js
+++ webpack_study/webpack_frist_not/webpack.config.js
@@ -1,3 +1,45 @@
+
+const path = require("path")
+
+const htmlplugin = require("html-webpack-plugin")
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+plugin = new htmlplugin({
+    // 指定要服务的页面
+    template:"./src/index.html",
+    // 复制到哪里去
+    filename:"./index.html"
+})
+
+
 module.exports = {
-    mode:"development"
+    mode:"development",
+    devtool:"eval-source-map",
+    plugins:[plugin,new CleanWebpackPlugin()],
+    output:{
+        path:path.join(__dirname,"./dist")
+    },
+    devServer:{
+        // 自动打开浏览器
+        open:true,
+        // 端口
+        port:10086
+    },
+    module:{
+        // 所有第三方文件模块的匹配规则
+        rules:[
+            // 文件后缀的匹配规则
+            // 处理css
+            {test:/\.css$/,use:["style-loader","css-loader"]},
+            // 处理less
+            {test: /\.less$/, use: ["style-loader", "css-loader", "less-loader",],
+            },
+        ]
+    },
+    resolve:{
+        alias:{
+            "@":path.join(__dirname,"./src/")
+        }
+    }
 }
