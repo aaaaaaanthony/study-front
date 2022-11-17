ad8832bc8035d906d0351d32aef472bd336b8263
OldYoung <mmzcg@outlook.com>
2022-11-17 18:43:08 +0800

    1

---
+++ items/daohang/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" style="height: 100%">
+    <HomeIndex></HomeIndex>
+  </div>
+</template>
+
+<script>
+import HomeIndex from "@/components/HomeIndex";
+
+export default {
+  name: 'App',
+  components: {
+    HomeIndex,
+  }
+}
+</script>
+
+<style>
+
+html,body{
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+
+/*body{*/
+/*  height: 100%;*/
+/*}*/
+
+</style>
